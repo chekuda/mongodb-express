c6262f484e0b2765786a4ced4a02b78e9c1e3f3d
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2018-01-12 16:54:36 +0000

    connect to local server

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+
+const db_URL = 'mongodb://127.0.0.1:27017'
 
 const app = express()
 
 const port = process.env.PORT || 5001
 
 app.get('/', (req, res) => res.send('CONNECTED'))
+
+app.get('/getdata', (req,res) => {
+  MongoClient.connect(db_URL, (err, db) => {
+    if (err) throw err
+
+    const dbo = db.db("vocabulary_test")
+
+    dbo.collection("vocabulary").find().toArray((err, result) => {
+      if (err) throw err
+
+      console.log(result)
+      db.close()
+    })
+  })
+})
 
 app.listen(port)
 
