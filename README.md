e31ebbf3bfbe83500ec22659911d4e937f15281c
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2018-01-14 22:41:41 +0000

    add mongoose for connect to database

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # MONGODB-EXPRESS
-Simple example how to connect my express api with mongo db
+
+Simple example how to connect my express api with mongo db. I am using mongoose in order to create the Schemas to save/read/delete data in the database.
+
+## Local database conection
+
+First please install mongodb in your computer:
+  - brew update
+  - brew mongodb
+  - Make sure you change the rights to write the db
+
+Second run mongodb to make the connection succesful:
+  - Open two terminals
+  - In one terminal run `mongod --bind_ip 127.0.0.1`
+  - In the other terminal run `mongo`
+
+## Remote database conection
+
+Set up the express server with the credentials provided by the remote database suplier
--- server.js
+++ server.js
@@ -1,29 +1,19 @@
 const express = require('express')
-const MongoClient = require('mongodb').MongoClient
+const mongoose = require('mongoose')
+const routes = require('./routes/routes')
 
-const db_URL = 'mongodb://127.0.0.1:27017'
+const db_URL = 'mongodb://127.0.0.1:27017/vocabulary_test'
+
+mongoose.connect(db_URL, { useMongoClient: true })
 
 const app = express()
 
 const port = process.env.PORT || 5001
 
+app.use('/api', routes.listRoutes)
+
 app.get('/', (req, res) => res.send('CONNECTED'))
-
-app.get('/getdata', (req,res) => {
-  MongoClient.connect(db_URL, (err, db) => {
-    if (err) throw err
-
-    const dbo = db.db("vocabulary_test")
-
-    dbo.collection("vocabulary").find().toArray((err, result) => {
-      if (err) throw err
-
-      console.log(result)
-      db.close()
-    })
-  })
-})
 
 app.listen(port)
 
-console.log(`Listening on port ${5001}`)
+console.log(`Listening on port ${port}`)
